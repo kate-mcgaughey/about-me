0319cd816a3f1e476d6ff00b13452d196244cc8a
McGaughey <therealmcgaughey@gmail.com>
2016-04-13 13:45:55 -0600

    Added header, body, and footer

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>About Me</title>
+	</head>
+	<body>
+		<h1>My Name is Kate McGaughey</h1>
+		<p>I'm the real McGaughey! After eight years in Santa Fe, I moved to Albuquerque to attend the Deep Dive Coding Bootcamp. When I graduate on June 17, 2016, I will be seeking work as a full-stack web developer.</p>
+		<img src="media/kate.jpg" alt="My picture"/> align="middle"
+		<h2>Some of my favorite things</h2>
+			<ul>
+				<li>Skiing</li>
+				<li>Mountain biking</li>
+				<li>Rock climbing</li>
+				<li>Backpacking in the wilderness</li>
+				<li>Dogs</li>
+				<li>Craft beer</li>
+				<li>Current events and journalism</li>
+				<li>Battlestar Galactica</li>
+			</ul>
+	</body>
+	<footer>
+		<small>© 2016 by Kate McGaughey, therealmcgaughey@gmail.com</small>
+	</footer>
+</html>
